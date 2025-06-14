b16cc382cfb4d138c8be54e97b3c936c981f48fc
AJ-Brown-InTech <ajalantbrown@gmail.com>
2025-06-14 17:59:57 -0500

    fix: file reader + add stream reader

--- lib/log_analyzer.ex
+++ lib/log_analyzer.ex
@@ -1,38 +1,36 @@
 defmodule LogAnalyzer do
-  @moduledoc """
-  Documentation for `LogAnalyzer`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> LogAnalyzer.hello()
-      Hello World
-
-  """
-  def hello do
-     IO.puts("Hello World")
+  
+  defp read_file(""), do: {:error, "file path is empty"}
+  defp read_file(path) when is_binary(path)  do
+     try do
+      fstream = File.stream!(path,[],:line)
+        IO.puts("Reading file...")
+        {:ok, fstream}
+     rescue
+       e in File.Error -> 
+       {:error, e.reason}    
+      end
   end
 
-  @doc """
-  open_file
-      - accepts a file path as a string
-    ## Examples
-     iex> LogAnalyzer.open_file("")
-     {:error, "file path is empty"} 
-        
-     iex> LogAnalyzer.open_file("example.txt")
-     {:ok, :contents...}
-  """
-  def open_file(""), do: {:error, "file path is empty"}
-
-  def open_file(path) do
-    File.read(path)
+  defp stream_reader(stream) do
+    Enum.each(stream, fn line ->
+        IO.puts("line -> #{String.trim(line)}")
+    end)
+    File.close(stream)  
   end
 
+  # TODO: add metrics 
+
+  def handle() do
+    case read_file("test.txt") do
+      {:ok, stream} ->
+        stream_reader(stream)
+      {:error, reason} ->
+        IO.puts("Failed to read file: #{reason}") 
+    end
+    
+  end
+  
 end
 
-
-LogAnalyzer.hello()
+ LogAnalyzer.handle()
